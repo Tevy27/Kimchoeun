42f85031d3b2a28af390eb2e286737fa56d51345
Tevy27 <Tevy27@users.noreply.github.com>
2024-01-01 07:42:20 +0000

    deploy: 873eb630b0bebc249d86bb59c77b268db6c923bf

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,32 @@ function downloadData() {
     window.location.href = 'viz/dataset.xlsx';
 }
 
-// Attach these functions to buttons
 document.getElementById('downloadReportBtn').addEventListener('click', downloadReport);
 document.getElementById('downloadDataBtn').addEventListener('click', downloadData);
+
+//script for dashboard 
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/dashboards.json')
+        .then(response => response.json())
+        .then(data => {
+            createDashboardButtons(data);
+        })
+        .catch(error => console.error(error));
+});
+
+function createDashboardButtons(dashboards) {
+    const buttonsContainer = document.getElementById('dashboardButtons');
+    dashboards.forEach(dashboard => {
+        const button = document.createElement('button');
+        button.textContent = dashboard.title;
+        button.classList.add('dashboard-button'); // Add CSS class for styling
+        button.onclick = () => loadDashboard(dashboard.url);
+        buttonsContainer.appendChild(button);
+    });
+}
+
+function loadDashboard(url) {
+    const iframe = document.getElementById('powerbiFrame');
+    iframe.src = url;
+}
+
